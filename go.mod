616124232f91a2e9a2723fde2339b1d8517ad019
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-21 08:34:05 -0400

    feat(cli): added cli menu for commit linking

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/JaleelB/jira-flow
 
 go 1.22.1
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- src/cli.go
+++ src/cli.go
@@ -1,17 +1,87 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"regexp"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
 )
 
-func cli() {
-	// Define flags
-	name := flag.String("name", "World", "a name to say hello to")
+func validateJiraKey(
+	input string, 
+	branchPattern string,
+	) (string, error) {
+	//the regex pattern for JIRA issue key validation
+	jiraIssueKeyPattern := regexp.MustCompile(branchPattern)
+	if !jiraIssueKeyPattern.MatchString(input) {
+		return "", fmt.Errorf("invalid JIRA issue key format. Valid format is <project_key>-<ticket_number> e.g. ABC-1234. Please try again")
+	}
+	return input, nil
+}
 
-	// Parse the flags
-	flag.Parse()
+func CreateMenu(
+	config *Config,
+) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "menu",
+		Short: "Create a CLI menu",
+		Long:  `This command creates a CLI menu using promptui.`,
+		Run: func(cmd *cobra.Command, args []string) {
 
-	// Use the flags
-	fmt.Printf("Hello, %s!\n", *name)
+			templates := &promptui.SelectTemplates{
+				Label:    "{{ . }}?",
+				Active:   "\U000027A1 {{ . | cyan }}", 
+				Inactive: "  {{ . | cyan }}",
+				Selected: "\u2714 {{ . | red }}", 
+			}
+
+			selectPrompt := promptui.Select{
+				Label: "How would you like to link your commits to a Jira issue?",
+				Items: []string{
+					"Automatically link commits to Jira issues",
+					"Manually link commits to Jira issues",
+				},
+				Templates: templates,
+			}
+
+			_, result, err := selectPrompt.Run()
+
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				return
+			}
+
+			switch result {
+				case "Automatically link commits to Jira issues":
+					config.AutoLink = true
+					fmt.Println("Automatic JIRA issue key tracking is enabled. The issue key will be extracted from the branch name and prepended to your commits, linking them to your JIRA issue.")
+					
+				case "Manually link commits to Jira issues":
+	
+					prompt := promptui.Prompt{
+						Label: "JIRA Issue Key",
+						Validate: func(input string) error {
+							_, err := validateJiraKey(input, config.BranchPattern)
+							return err
+						},
+					}
+
+					result, err := prompt.Run()
+
+					if err != nil {
+						fmt.Printf("Prompt failed %v\n", err)
+						return
+					}
+
+					config.JiraKey = result
+					config.AutoLink = false
+
+					fmt.Printf("You have entered JIRA issue key: %q\n. The issue key will now prepeded to your commmits linking them to your JIRA issue.", result)
+			
+			}
+		},
+	}
+
+	return cmd
 }
