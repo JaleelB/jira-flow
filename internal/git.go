dcc0c8c7378ac831d054bd07a25e8b83e3d12837
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-24 15:29:08 -0400

    fix(git): check if directory is writable by attempting to create a temporary file.

--- internal/git.go
+++ internal/git.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,53 +18,89 @@ func ExecuteGitCommand(args ...string) (string, error) {
 	return strings.TrimSuffix(string(output), "\n"), nil
 }
 
-func SetGitHookScript(config *Config) error {
-	// Hook script content
-	hookScript := `
-		#!/bin/sh
-		# This hook will prepend the JIRA issue key to commit messages
+// func SetGitHookScript(config *Config) error {
+// 	// Hook script content
+// 	hookScript := `
+// 		#!/bin/sh
+// 		# This hook will prepend the JIRA issue key to commit messages
 
-		# Get the name of the current branch
-		BRANCH_NAME=$(git symbolic-ref --short HEAD)
+// 		# Get the name of the current branch
+// 		BRANCH_NAME=$(git symbolic-ref --short HEAD)
 
-		# Define the regex to match the JIRA issue ID
-		JIRA_ISSUE_REGEX='[A-Z]+-[0-9]+'
+// 		# Define the regex to match the JIRA issue ID
+// 		JIRA_ISSUE_REGEX='[A-Z]+-[0-9]+'
 
-		# Extract the JIRA issue ID from the branch name using the regex
-		if [[ $BRANCH_NAME =~ $JIRA_ISSUE_REGEX ]]; then
-			JIRA_ISSUE_ID=${BASH_REMATCH[0]}
-		else
-			# If not found in branch name, use manual input (if provided)
-			JIRA_ISSUE_ID='%s'
-		fi
+// 		# Extract the JIRA issue ID from the branch name using the regex
+// 		if [[ $BRANCH_NAME =~ $JIRA_ISSUE_REGEX ]]; then
+// 			JIRA_ISSUE_ID=${BASH_REMATCH[0]}
+// 		else
+// 			# If not found in branch name, use manual input (if provided)
+// 			JIRA_ISSUE_ID='%s'
+// 		fi
 
-		# Prepend the JIRA issue ID to the commit message
-		if [[ -n "$JIRA_ISSUE_ID" ]]; then
-			sed -i.bak -e "1s/^/$JIRA_ISSUE_ID /" "$1"
-		else
-			echo "JIRA issue key not provided or extracted. Commit message not modified."
-		fi
-	`
+// 		# Prepend the JIRA issue ID to the commit message
+// 		if [[ -n "$JIRA_ISSUE_ID" ]]; then
+// 			sed -i.bak -e "1s/^/$JIRA_ISSUE_ID /" "$1"
+// 		else
+// 			echo "JIRA issue key not provided or extracted. Commit message not modified."
+// 		fi
+// 	`
 
-	// Write the hook script to the hook path
-	// hookPath := fmt.Sprintf("%sprepare-commit-msg", config.HookPath)
-	hookPath := "."
-	err := os.WriteFile(hookPath, []byte(fmt.Sprintf(hookScript, config.JiraKey)), 0755)
-	if err != nil {
-		return fmt.Errorf("os.WriteFile: %v", err)
-	}
+// 	// Write the hook script to the hook path
+// 	// hookPath := fmt.Sprintf("%sprepare-commit-msg", config.HookPath)
+// 	hookPath := "."
+// 	// hookFileName := "pre-commit" // change this to the appropriate hook name
+// 	// hookPath = filepath.Join(config.HookPath, hookFileName)
 
-	// Make the hook script executable
-	_, err = ExecuteGitCommand("add", "--chmod=+x", hookPath)
-	if err != nil {
-		return fmt.Errorf("git add --chmod=+x: %v", err)
-	}
+// 	err := os.WriteFile(hookPath, []byte(fmt.Sprintf(hookScript, config.JiraKey)), 0755)
+// 	if err != nil {
+// 		return fmt.Errorf("os.WriteFile: %v", err)
+// 	}
 
-	_, err = ExecuteGitCommand("update-index", "--add", "--chmod=+x", hookPath)
-	if err != nil {
-		return fmt.Errorf("git update-index --add --chmod=+x: %v", err)
-	}
+// 	// // Make the hook script executable
+// 	// _, err = ExecuteGitCommand("add", "--chmod=+x", hookPath)
+// 	// if err != nil {
+// 	// 	return fmt.Errorf("git add --chmod=+x: %v", err)
+// 	// }
 
-	fmt.Println("\nGit hook installed successfully.")
-	return nil
+// 	// _, err = ExecuteGitCommand("update-index", "--add", "--chmod=+x", hookPath)
+// 	// if err != nil {
+// 	// 	return fmt.Errorf("git update-index --add --chmod=+x: %v", err)
+// 	// }
+
+// 	fmt.Println("\nGit hook installed successfully.")
+// 	return nil
+// }
+
+func CheckGitAndHooksDir() error {
+    // Check if git is installed
+    _, err := exec.LookPath("git")
+    if err != nil {
+        return fmt.Errorf("git is not installed: %w", err)
+    }
+
+    // Check if the script is being run from within a git repository
+    gitRoot, err := ExecuteGitCommand("rev-parse", "--show-toplevel")
+    if err != nil {
+        return fmt.Errorf("this doesn't seem to be a git repository: %w", err)
+    }
+
+    // Construct the full path to the .git/hooks directory
+    hooksDir := filepath.Join(gitRoot, ".git", "hooks")
+
+    // Test writability by creating and removing a temporary file
+    testFilePath := filepath.Join(hooksDir, ".tmp-hook-test")
+    testFile, err := os.Create(testFilePath)
+    if err != nil {
+        return fmt.Errorf("failed to create a file in the hooks directory to test writability: %w", err)
+    }
+    testFile.Close()
+
+    // Clean up the temporary file
+    err = os.Remove(testFilePath)
+    if err != nil {
+        return fmt.Errorf("failed to remove the temporary file in the hooks directory: %w", err)
+    }
+
+    return nil
 }
