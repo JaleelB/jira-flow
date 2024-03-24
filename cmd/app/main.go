fa22b14d289059275bfcbf98f73f7ea7617f0db4
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-23 22:04:57 -0400

    fix(app): fixing package import path

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/JaleelB/jira-flow/internal"
 	"github.com/spf13/cobra"
 )
 
 func main() {
 
-	config := NewConfig()  // create a new config object
+	config := internal.NewConfig()  // create a new config object
 
 	rootCmd := &cobra.Command{
 		Use: "myapp",
 		Run: func(cmd *cobra.Command, args []string) {
 			// run the menu command by default, passing in the config
-			menuCmd := CLIMenu(config)
+			menuCmd := internal.CLIMenu(config)
 			menuCmd.Execute()
 		},
 	}
