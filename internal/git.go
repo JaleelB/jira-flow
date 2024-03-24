0c85e9b4d972f6e3dd52f794a535d502dd6eec24
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-24 15:32:24 -0400

    fix(git): removed executing git command to change permission

--- internal/git.go
+++ internal/git.go
@@ -18,59 +18,46 @@ func ExecuteGitCommand(args ...string) (string, error) {
 	return strings.TrimSuffix(string(output), "\n"), nil
 }
 
-// func SetGitHookScript(config *Config) error {
-// 	// Hook script content
-// 	hookScript := `
-// 		#!/bin/sh
-// 		# This hook will prepend the JIRA issue key to commit messages
+func SetGitHookScript(config *Config) error {
+	// Hook script content
+	hookScript := `
+		#!/bin/sh
+		# This hook will prepend the JIRA issue key to commit messages
 
-// 		# Get the name of the current branch
-// 		BRANCH_NAME=$(git symbolic-ref --short HEAD)
+		# Get the name of the current branch
+		BRANCH_NAME=$(git symbolic-ref --short HEAD)
 
-// 		# Define the regex to match the JIRA issue ID
-// 		JIRA_ISSUE_REGEX='[A-Z]+-[0-9]+'
+		# Define the regex to match the JIRA issue ID
+		JIRA_ISSUE_REGEX='[A-Z]+-[0-9]+'
 
-// 		# Extract the JIRA issue ID from the branch name using the regex
-// 		if [[ $BRANCH_NAME =~ $JIRA_ISSUE_REGEX ]]; then
-// 			JIRA_ISSUE_ID=${BASH_REMATCH[0]}
-// 		else
-// 			# If not found in branch name, use manual input (if provided)
-// 			JIRA_ISSUE_ID='%s'
-// 		fi
+		# Extract the JIRA issue ID from the branch name using the regex
+		if [[ $BRANCH_NAME =~ $JIRA_ISSUE_REGEX ]]; then
+			JIRA_ISSUE_ID=${BASH_REMATCH[0]}
+		else
+			# If not found in branch name, use manual input (if provided)
+			JIRA_ISSUE_ID='%s'
+		fi
 
-// 		# Prepend the JIRA issue ID to the commit message
-// 		if [[ -n "$JIRA_ISSUE_ID" ]]; then
-// 			sed -i.bak -e "1s/^/$JIRA_ISSUE_ID /" "$1"
-// 		else
-// 			echo "JIRA issue key not provided or extracted. Commit message not modified."
-// 		fi
-// 	`
+		# Prepend the JIRA issue ID to the commit message
+		if [[ -n "$JIRA_ISSUE_ID" ]]; then
+			sed -i.bak -e "1s/^/$JIRA_ISSUE_ID /" "$1"
+		else
+			echo "JIRA issue key not provided or extracted. Commit message not modified."
+		fi
+	`
 
-// 	// Write the hook script to the hook path
-// 	// hookPath := fmt.Sprintf("%sprepare-commit-msg", config.HookPath)
-// 	hookPath := "."
-// 	// hookFileName := "pre-commit" // change this to the appropriate hook name
-// 	// hookPath = filepath.Join(config.HookPath, hookFileName)
+	// Write the hook script to the hook path
+	hookFileName := "pre-commit" 
+	hookPath := filepath.Join(config.HookPath, hookFileName)
 
-// 	err := os.WriteFile(hookPath, []byte(fmt.Sprintf(hookScript, config.JiraKey)), 0755)
-// 	if err != nil {
-// 		return fmt.Errorf("os.WriteFile: %v", err)
-// 	}
+	err := os.WriteFile(hookPath, []byte(fmt.Sprintf(hookScript, config.JiraKey)), 0755)
+	if err != nil {
+		return fmt.Errorf("os.WriteFile: %v", err)
+	}
 
-// 	// // Make the hook script executable
-// 	// _, err = ExecuteGitCommand("add", "--chmod=+x", hookPath)
-// 	// if err != nil {
-// 	// 	return fmt.Errorf("git add --chmod=+x: %v", err)
-// 	// }
-
-// 	// _, err = ExecuteGitCommand("update-index", "--add", "--chmod=+x", hookPath)
-// 	// if err != nil {
-// 	// 	return fmt.Errorf("git update-index --add --chmod=+x: %v", err)
-// 	// }
-
-// 	fmt.Println("\nGit hook installed successfully.")
-// 	return nil
-// }
+	fmt.Println("\nGit hook installed successfully.")
+	return nil
+}
 
 func CheckGitAndHooksDir() error {
     // Check if git is installed
