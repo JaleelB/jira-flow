536b31afc1efc5110452cdd1695dfa46a4a2ed8b
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-26 08:39:29 -0400

    feat(script): implemented script to build cross platfor binaries

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the output directory for binaries
+BIN_DIR="../bin"
+mkdir -p $BIN_DIR
+
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
+# Build for Linux
+env GOOS=linux GOARCH=amd64 go build -o ${BIN_DIR}/commitmsg-linux commitmsg.go
+
+# Build for macOS
+env GOOS=darwin GOARCH=amd64 go build -o ${BIN_DIR}/commitmsg-macos commitmsg.go
+
+# Build for Windows
+env GOOS=windows GOARCH=amd64 go build -o ${BIN_DIR}/commitmsg-windows.exe commitmsg.go
+
+echo "Binaries compiled to ${BIN_DIR}"
