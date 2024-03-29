05f8e021d3e1fb777fe63dd778878c673ce72ce5
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-03-28 23:38:21 -0400

    feat(cli): enhance cli with color accents and ui changes

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 )
--- internal/cli.go
+++ internal/cli.go
@@ -2,10 +2,17 @@ package internal
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/logrusorgru/aurora"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
+
+type options struct {
+	Name string
+	Context string
+}
 
 func CLIMenu(
 	config *Config,
@@ -13,47 +20,85 @@ func CLIMenu(
 
 	jiraManager := NewJiraManager(config)
 
+	logo := `
+     __ ____ ___   ___    ____ __   ____  _      __
+ __ / //  _// _ \ / _ |  / __// /  / __ \| | /| / /
+/ // /_/ / / , _// __ | / _/ / /__/ /_/ /| |/ |/ / 
+\___//___//_/|_|/_/ |_|/_/  /____/\____/ |__/|__/                                                                  
+	`
+
 	cmd := &cobra.Command{
-		Use:   "menu",
-		Short: "Create a CLI menu",
-		Long:  `This command creates a CLI menu using promptui.`,
+		Use:   "jira-flow init",
+		Short: "Interact with Jira from your command line",
+		Long:  `JiraFlow lets you link your commits with JIRA tickets directly from your command line.`,
 		Run: func(cmd *cobra.Command, args []string) {
 
+			fmt.Printf("%s", aurora.BrightYellow(logo))
+
+			description := aurora.BrightMagenta("\nWelcome to JiraFlow! JiraFlow will help you link your commits with JIRA tickets directly from your command line.\n")
+			fmt.Println(description)
+
+			options := []options{
+				{Name: "Automatically", Context: "Automatically link commits to Jira issues based on branch name"},
+				{Name: "Manually", Context: "Manually link commits to Jira issues by entering the Jira issue key"},
+			}
+
 			templates := &promptui.SelectTemplates{
-				Label:    "{{ . }}?",
-				Active:   "\U000027A1 {{ . | cyan }}", 
-				Inactive: "  {{ . | cyan }}",
-				Selected: "\u2714 {{ . | red }}", 
+				Label:    "\u003F {{ . }}",
+				Active:   "\u27A4 {{ .Name | cyan }}", 
+				Inactive: "  {{ .Name | white }}",
+				Selected: "\u2713 How would you like to link your commits to a Jira issue: {{ .Context }}", 
+				Details: `
++ {{ .Context | faint }}
+`,
+			}
+
+			searcher := func(input string, index int) bool {
+				option := options[index]
+				name := strings.Replace(strings.ToLower(option.Name), " ", "", -1)
+				input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+				return strings.Contains(name, input)
 			}
 
 			selectPrompt := promptui.Select{
 				Label: "How would you like to link your commits to a Jira issue?",
-				Items: []string{
-					"Automatically link commits to Jira issues",
-					"Manually link commits to Jira issues",
-				},
+				Items: options,
 				Templates: templates,
+				Size: 4,
+				Searcher: searcher,
 			}
 
-			_, result, err := selectPrompt.Run()
+			idx, _, err := selectPrompt.Run()
 
 			if err != nil {
 				fmt.Printf("Prompt failed %v\n", err)
 				return
 			}
 
-			switch result {
-				case "Automatically link commits to Jira issues":
+			selectedOption := options[idx]
+
+			switch selectedOption.Name {
+				case "Automatically":
 					jiraManager.Config.AutoLink = true
-					fmt.Println("Automatic JIRA issue key tracking is enabled. The issue key will be extracted from the branch name and prepended to your commits, linking them to your JIRA issue.")
+
+					// msg := fmt.Sprintf("\nYou have entered JIRA issue key: %q. The issue key will now prepeded to your commmits linking them to your JIRA issue.", result)
+					// fmt.Printf("%s", aurora.BrightMagenta(msg))
+					fmt.Println("\nAutomatic JIRA issue key tracking is enabled. The issue key will be extracted from the branch name and prepended to your commits, linking them to your JIRA issue.")
 					
-				case "Manually link commits to Jira issues":
+				case "Manually":
 
 					prompt := promptui.Prompt{
-						Label: "JIRA Issue Key",
+						Label: "JIRA Issue Key: ",
 						Validate: func(input string) error {
 							_, err := jiraManager.ValidateJiraKey(input)
 							return err
+						},
+						Templates: &promptui.PromptTemplates{
+							Prompt:  "\u003F {{ . }}",
+							Valid:   "\u003F {{ . | white }}",
+							Invalid: "\u003F {{ . | white }}",
+							Success: "\u2713 {{ . | white }}",
 						},
 					}
 
@@ -69,18 +114,18 @@ func CLIMenu(
 
 					checkDirErr := CheckGitAndHooksDir()
 					if checkDirErr != nil {
-						fmt.Printf("Failed to check git and hooks directory: %v\n", checkDirErr)
+						fmt.Printf("\nFailed to check git and hooks directory: %v\n", checkDirErr)
 						return
 					}
 
 					setHookErr := SetGitHookScript(jiraManager.Config)
 					if setHookErr != nil {
-						fmt.Printf("Failed to set git hook script: %v\n", err)
+						fmt.Printf("\nFailed to set git hook script: %v\n", err)
 						return
 					}
 					
-					fmt.Printf("You have entered JIRA issue key: %q\n. The issue key will now prepeded to your commmits linking them to your JIRA issue.", result)
-			
+					msg := fmt.Sprintf("\nYou have entered JIRA issue key: %q. The issue key will now prepeded to your commmits linking them to your JIRA issue.", result)
+					fmt.Printf("%s", aurora.BrightMagenta(msg))			
 			}
 		},
 	}
