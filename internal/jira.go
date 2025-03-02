e8564d851d34fee1329fb23a2d77a9f8e3ec4a52
Jaleel Bennett <jaleelbennett58@gmail.com>
2025-03-02 11:54:00 -0500

    added automatic JIRA issue key extraction configuration

--- internal/jira.go
+++ internal/jira.go
@@ -29,5 +29,31 @@ func (jm *JiraManager) ValidateJiraKey(
 	return input, nil
 }
 
+// ConfigureAutomatic sets up automatic JIRA issue key extraction from branch names
+func (jm *JiraManager) ConfigureAutomatic() error {
+	// Set the config to use automatic linking
+	jm.Config.AutoLink = true
+
+	// Try to extract the JIRA key from the current branch as a test
+	branchName, err := GetCurrentBranchName()
+	if err != nil {
+		return fmt.Errorf("failed to get current branch name: %w", err)
+	}
+
+	// Don't fail if we're on main/master
+	if branchName == "main" || branchName == "master" {
+		fmt.Println("Currently on", branchName, "branch. JiraFlow will extract JIRA keys when you switch to feature branches.")
+		return nil
+	}
+
+	// For other branches, try to extract the key as a test
+	_, err = jm.ExtractIssueKeyFromBranchName(branchName)
+	if err != nil {
+		return fmt.Errorf("failed to extract JIRA issue key from branch name: %w", err)
+	}
+
+	return nil
+}
+
 
 
